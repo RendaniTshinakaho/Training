0f061d55e52f53ca8d42f1acdf74d2d79e82a9be
rendanitshinakaho <r.nkhumeleni@gmail.com>
2013-08-15 18:20:47 +0200

    AdoNet Files

---
+++ AdoNet.DataLayer/Repository/SimpleRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using DomainClasses;
+
+namespace Repository
+{
+    public class SimpleRepository
+    {
+        public void getStudent()
+        {
+            var context = new StudentDatabaseTestEntities();
+            var cust = from c in context.Students select c;
+            foreach (var student in cust)
+            {
+                Console.WriteLine("{0}", student.Name);
+                Console.WriteLine("{0}",student.StudentId.ToString(CultureInfo.InvariantCulture));
+                Console.WriteLine("{0}",student.Subjects);
+                Console.ReadLine();
+            }
+     }
+    }
+}
---
+++ InversionOfControll/InversionOfControll.App/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Ninject;
+using Ninject.Modules;
+
+namespace InversionOfControll.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kernel = new StandardKernel(new SwordInject());
+            var sam = kernel.Get<IWarrior>();
+            sam.Attack("the unwashed");
+            Console.ReadKey();
+        }
+    }
+
+    internal interface IWarrior
+    {
+        void Attack(string target);
+    }
+
+    class Samurai : IWarrior
+    {
+        private Sword _sword;
+        [Inject]
+        public Samurai(Sword sword)
+        {
+            _sword = sword;
+        }
+
+        public void Attack(string target)
+        {
+            _sword.Hit(target);
+        }
+    }
+
+    internal interface IWeapon
+    {
+        void Hit(string target);
+    }
+
+    internal class Sword : IWeapon
+    {
+        public void Hit(string target)
+        {
+            Console.WriteLine("Chopped {0} into halves",target);
+        }
+    }
+
+    class Shuriken
+    {
+        public void Hit(string target)
+        {
+            Console.WriteLine("Pieces {0} armor",target);
+        }
+    }
+
+    class SwordInject:NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IWarrior>().To<Samurai>().InSingletonScope();
+            Bind<IWeapon>().To<Sword>().InSingletonScope();
+        }
+    }
+}
